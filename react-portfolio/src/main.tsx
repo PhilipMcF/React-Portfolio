a1a973e73a8aad2af8cd036f492c1fab8d19cbd0
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-09-22 00:30:48 -0400

    Added pages components. Added CSS from old module. Changed main.tsx.

--- react-portfolio/src/main.tsx
+++ react-portfolio/src/main.tsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.tsx'
+// import About from './pages/about.tsx'
+// import Contact from './pages/contact.tsx'
+// import Portfolio from './pages/portfolio.tsx'
+// import Resume from './pages/resume.tsx'
+// import Error from './pages/error.tsx'
+
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <Error />
+    children:[
+      {
+        index: true,
+        // element: <About />
+      },
+      {
+
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />
+);
