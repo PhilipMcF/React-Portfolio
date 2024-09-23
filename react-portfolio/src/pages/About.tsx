650cc67141dfcad1d547797034227f1063163ff4
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-09-22 21:20:07 -0400

    made changes to about page content.

--- react-portfolio/src/pages/About.tsx
+++ react-portfolio/src/pages/About.tsx
@@ -3,8 +3,23 @@ import { BsPersonFill } from "react-icons/bs";
 
 export default function About () {
     return (
-        <div>
+        <div className="aboutD">
             <h1>About Me <BsPersonFill size={50} style={styles}/> </h1> 
+            <br></br>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Earum unde explicabo a magnam dolorem quia dolore quas eos
+                culpa asperiores natus, perferendis temporibus ipsa velit tempore ex facilis, quo animi!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum perferendis aperiam cum,
+                quos laborum vero, distinctio ab a quia quae cupiditate dolorem ex veniam consectetur,
+                itaque architecto eum culpa dolores.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Earum unde explicabo a magnam dolorem quia dolore quas eos
+                culpa asperiores natus, perferendis temporibus ipsa velit tempore ex facilis, quo animi!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum perferendis aperiam cum,
+                quos laborum vero, distinctio ab a quia quae cupiditate dolorem ex veniam consectetur,
+                itaque architecto eum culpa dolores.
+            </p>
             <br></br>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
