ddf2f2dc96aef05209951b38ba4865166be6aacb
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-09-23 17:54:26 -0400

    completed Resume.tsx page

--- react-portfolio/src/pages/Resume.tsx
+++ react-portfolio/src/pages/Resume.tsx
@@ -2,6 +2,30 @@ export default function Resume() {
     return (
         <section className="resumeS">
             <h1>Resume</h1>
+            <p>
+                <a href="https://www.linkedin.com/in/philip-mcfarland-aa3b4a191/" target="_blank" rel="noopener noreferrer">
+                    Download my resume here!
+                </a>
+            </p>
+            <br></br>
+            <h1>Front-end Proficiencies</h1>
+            <ul className="resumeList">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>TypeScript</li>
+                <li>React</li>
+                <li>Materialize CSS</li>
+            </ul>
+            <br></br>
+            <h1>Back-end Proficiencies</h1>
+            <ul className="resumeList">
+                <li>APIs</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>PostgreSQL</li>
+                <li>REST</li>
+            </ul>
         </section>
     )
 }
