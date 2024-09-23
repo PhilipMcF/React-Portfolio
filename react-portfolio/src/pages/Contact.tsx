fda90c596afdcc9b35e5ce642c4de0dc2c5eb068
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-09-22 22:46:05 -0400

    completed contact page

--- react-portfolio/src/pages/Contact.tsx
+++ react-portfolio/src/pages/Contact.tsx
@@ -1,4 +1,99 @@
 import { useState } from "react";
+import {validateEmail} from "../utils/helpers";
 
+function Contact() {
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const { name, email, message } = formState;
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!errorMessage) {
+            console.log('Submit Form', formState);
+        }
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } 
+            else {
+                setErrorMessage('');
+            }
+        } 
+        else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } 
+            else {
+                setErrorMessage('');
+            }
+        }
+        if (!errorMessage) {
+            const updatedFormState = { ...formState, [e.target.name]: e.target.value }; //for logging purposes
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+            console.log('Handle Form', updatedFormState);
+        }
+    };
+
+    function renderErrorMessage() {
+        if (errorMessage) {
+            return (
+                <div>
+                <p className="error">{errorMessage}</p>
+                </div>
+            );
+        }
+        return null;
+    }
+
+    return (
+        <section className="contactS">
+            <h2>Contact</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <br></br>
+                    <input className="input-field"
+                        type="text"
+                        name="name"
+                        defaultValue={name}
+                        onBlur={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <br></br>
+                    <input className="input-field"
+                    type="email"
+                    name="email"
+                    defaultValue={email}
+                    onBlur={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <br></br>
+                    <textarea className="textarea-field"
+                        name="message"
+                        rows={7}
+                        defaultValue={message}
+                        onBlur={handleChange}
+                    />
+                </div>
+                {renderErrorMessage()}
+                <button type="submit">Submit</button>
+            </form>
+        </section>
+    );
+}
 
 // export default Contact;
+export default Contact;
