cf5c57fdc196b7763a8d53fdd63be0f05eb939b0
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-09-22 23:07:04 -0400

    added in code from module #2 work. need to replace placeholders with actual images and link to github repo projects.

---
+++ react-portfolio/src/pages/Portfolio.tsx
@@ -0,0 +1,28 @@
+export default function Portfolio() {
+    return (
+        <section className="page-section" id="work">
+            <h2>Portfolio</h2>
+            <div className="flex-container">
+                <div className="flex-item" id="firstproject"
+                    title="First project - image of sign with man sitting on desk working on computer.">
+                    <h3>Placeholder #1</h3>
+                </div>
+                <div className="flex-item" id="secondproject" title="Second project - image of text that says work in progress.">
+                    <h3>Placeholder #2</h3>
+                </div>
+                <div className="flex-item" id="thirdproject"
+                    title="Third project - image with the text work in progress and a caution sign.">
+                    <h3>Placeholder #3</h3>
+                </div>
+                <div className="flex-item" id="thirdproject"
+                    title="Fourth project - image with the text work in progress and a caution sign.">
+                    <h3>Placeholder #4</h3>
+                </div>
+                <div className="flex-item" id="thirdproject"
+                    title="Fifth project - image with the text work in progress and a caution sign.">
+                    <h3>Placeholder #5</h3>
+                </div>
+            </div>
+        </section>
+    )
+}
