a3e33e580357845c88b2df6cbad431aa19fc830a
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-09-24 00:20:47 -0400

    added page transition for Resume.tsx

--- react-portfolio/src/pages/Resume.tsx
+++ react-portfolio/src/pages/Resume.tsx
@@ -1,31 +1,38 @@
-export default function Resume() {
+import React from 'react';
+import PageTransition from '../components/PageTransition'; // Adjust the path if necessary
+
+const Resume: React.FC = () => {
     return (
-        <section className="resumeS">
-            <h1>Resume</h1>
-            <p>
-                <a href="https://www.linkedin.com/in/philip-mcfarland-aa3b4a191/" target="_blank" rel="noopener noreferrer">
-                    Download my resume here!
-                </a>
-            </p>
-            <br></br>
-            <h1>Front-end Proficiencies</h1>
-            <ul className="resumeList">
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>TypeScript</li>
-                <li>React</li>
-                <li>Materialize CSS</li>
-            </ul>
-            <br></br>
-            <h1>Back-end Proficiencies</h1>
-            <ul className="resumeList">
-                <li>APIs</li>
-                <li>Node.js</li>
-                <li>Express.js</li>
-                <li>PostgreSQL</li>
-                <li>REST</li>
-            </ul>
-        </section>
-    )
-}
+        <PageTransition>
+            <section className="resumeS">
+                <h1>Resume</h1>
+                <p>
+                    <a href="https://www.linkedin.com/in/philip-mcfarland-aa3b4a191/" target="_blank" rel="noopener noreferrer">
+                        Download my resume here!
+                    </a>
+                </p>
+                <br />
+                <h1>Front-end Proficiencies</h1>
+                <ul className="resumeList">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>React</li>
+                    <li>Materialize CSS</li>
+                </ul>
+                <br />
+                <h1>Back-end Proficiencies</h1>
+                <ul className="resumeList">
+                    <li>APIs</li>
+                    <li>Node.js</li>
+                    <li>Express.js</li>
+                    <li>PostgreSQL</li>
+                    <li>REST</li>
+                </ul>
+            </section>
+        </PageTransition>
+    );
+};
+
+export default Resume;
